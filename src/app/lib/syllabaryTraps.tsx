18c840b112411b9cabfb5554043f9f4beafe3396
hermelen <hermelen.peris@gmail.com>
2024-11-07 13:04:21 +0100

    list traps

---
+++ src/app/lib/syllabaryTraps.tsx
@@ -0,0 +1,13 @@
+export const SyllabaryTraps = [
+    ["u", "tsu", "dzu"],
+    ["sa", "za", "chi", "ra", "dji"],
+    ["ta", "na"],
+    ["ke", "ha", "ho", "ge", "ba", "bo", "pa", "po"],
+    ["hi", "bi", "pi"],
+    ["he", "be", "pe"],
+    ["ku", "gu"],
+    ["fu", "bu", "pu"],
+    ["ne", "re", "wa"],
+    ["nu", "no", "me"],
+    ["se", "ze"]
+]
